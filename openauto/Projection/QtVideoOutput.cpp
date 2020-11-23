b3f9b0a09d6779a60adcdc8a0a0da4ec563470c5
Cacaonut <benedikt.kaesen@avalanche-assistant.com>
2020-11-23 14:57:14 +0100

    Update QtVideoOutput.cpp

--- openauto/Projection/QtVideoOutput.cpp
+++ openauto/Projection/QtVideoOutput.cpp
@@ -80,7 +80,6 @@ void QtVideoOutput::onStartPlayback()
         videoWidget_->setAspectRatioMode(Qt::IgnoreAspectRatio);
         videoWidget_->setFocus();
         videoWidget_->resize(720, 435);
-        videoWidget_->move(0, 75);
         videoWidget_->showMinimized();
     }
     else
